dc48d6edae3d7e6242fa4859a8d47edbce97657c
grantimbo <grant.imbo@gmail.com>
2020-10-24 02:14:52 +0800

    new icon, revert logo

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 		<meta charset="<?php bloginfo('charset'); ?>">
 		<title>Monchel Sharp</title>
 
-		<link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.png" rel="shortcut icon">
+		<link href="<?php echo get_template_directory_uri(); ?>/img/icons/icon.png" rel="shortcut icon">
 
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
